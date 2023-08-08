979e97383639eae69c0b2a73516415b55685b012
elkihonge <kingkihonge@gmail.com>
2023-08-08 09:39:54 +0300

    variables

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+       //variables
+    var name = " Kim"
+    var age = 78
+    println(name)
+    println(age)
+
+    var city:String = "Nairobi"
+    print(city)
+    var idnum:Int = 41881572
+    print(idnum)
+
+
+
+
+
+
+
+}
---
+++ src/main/kotlin/var file.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+    var num1:Int = 20
+    var num2 = 15
+
+    var result1 = num1 + num2
+    println(result1)
+
+   var num3= 12
+    var num4 = 10
+
+    var result2 = num3 * num4
+    println(result2)
+
+    var result3 = num3/num4
+    println(result3)
+
+    var result4 = num3 - num4
+    println(result4)
+
+}
